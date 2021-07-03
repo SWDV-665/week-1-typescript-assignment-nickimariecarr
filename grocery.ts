b97fe65e75ace3ffa880edd3eff23ab15b242fb7
nickimariecarr <80299874+nickimariecarr@users.noreply.github.com>
2021-07-03 19:00:49 -0400

    Constructor addition
    Added to constructor and made minor changes to the class. Quantity in class was string, and should have been number. Corrected.

--- grocery.ts
+++ grocery.ts
@@ -1,9 +1,15 @@
 class Grocery{
     name: string;
     SKU: string;
-    quantity: string;
+    quantity: number;
     price: number;
     stock: string;
 
-    constructor(name:string, sku:string, quantity:number,price:number,stock:string){}
+    constructor(name:string, sku:string, quantity:number,price:number,stock:string){
+        this.name=name;
+        this.SKU=sku;
+        this.quantity=quantity;
+        this.price=price;
+        this.stock=stock;
+    }
 }
