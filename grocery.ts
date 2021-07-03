dbe40869c144b45acdfd56ebcbbe5c315059a12c
nickimariecarr <80299874+nickimariecarr@users.noreply.github.com>
2021-07-03 19:02:58 -0400

    Added groceryList items
    Added the grocery list items with item, SKU, quantity, price, and if it is in stock or out of stock

--- grocery.ts
+++ grocery.ts
@@ -13,3 +13,12 @@ class Grocery{
         this.stock=stock;
     }
 }
+
+let groceryList=[
+    new Grocery("Bread","T6745", 1, 1.59,"In Stock"),
+    new Grocery("Dozen Eggs","R4582",1,1.89,"In Stock"),
+    new Grocery("Flour","O7742",1,2.79,"Out of Stock"),
+    new Grocery("Milk","W8321",1,2.69,"In Stock"),
+    new Grocery("Butter","E3475",1,2.99,"Out of Stock")
+
+]
